b368fb1de494eeb9364456a1b766d9dec142f571
tiffanyvuu <tiffanyvuu17@gmail.com>
2023-05-10 14:38:49 -0400

    debugging sidebar

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function openSidebar() {
     document.getElementById("sidebar").style.display = "block";
     document.getElementById("sidebar-arrow-left").style.display = "block";
     document.getElementById("sidebar-arrow-right").style.display = "none";
-    if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("sidebar", "opened");
+    if (typeof(localStorage) !== "undefined") {
+        localStorage.setItem("sidebarStatus", "opened");
     }
 }
 
@@ -15,17 +15,17 @@ function closeSidebar() {
     document.getElementById("sidebar").style.display = "none";
     document.getElementById("sidebar-arrow-left").style.display = "none";
     document.getElementById("sidebar-arrow-right").style.display = "block";
-    if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("sidebar", "closed");
+    if (typeof(localStorage) !== "undefined") {
+        localStorage.setItem("sidebarStatus", "closed");
     }
 }
 
 function sidebarState() {
-    if (typeof(Storage) !== "undefined") {
-        if (localStorage.getItem("sidebar") === "opened") {
+    if (typeof(localStorage) !== "undefined") {
+        if (localStorage.getItem("sidebarStatus") === "opened") {
             openSidebar();
         }
-        else {
+        else if (localStorage.getItem("sidebarStatus") === "closed") {
             closeSidebar();
         }
     }
